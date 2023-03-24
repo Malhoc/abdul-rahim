5fbf4852d2633d00dbb39ba49a11ea4114a449df
Maria Anwar <mariaanwar996@gmail.com>
2023-03-24 21:30:52 +0500

    views-pages created

---
+++ resources/views/pages/aboutUs.blade.php
@@ -0,0 +1,230 @@
+<!-- Page Title #1
+============================================= -->
+<section id="page-title" class="page-title">
+	<div class="container-fluid bg-overlay bg-overlay-theme">
+		<div class="bg-section">
+			<img src="assets/images/page-title/title-1.jpg" alt="Background"/>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12">
+				<div class="title title-1 text--center">
+					<div class="title--content">
+						<div class="title--heading">
+							<h1>about</h1>
+						</div>
+					</div>
+				</div><!-- .page-title end -->
+			</div><!-- .col-md-12 end -->
+		</div><!-- .row end -->
+	</div><!-- .container end -->
+</section><!-- #page-title end -->
+<div class="clearfix mb-150"></div>
+<!-- About #1
+============================================= -->
+<section id="about" class="about-1 bg-white">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<div class="accordion accordion-1" id="accordion01">
+					<!-- Panel 01 -->
+					<div class="panel">
+						<div class="panel--heading">
+							<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion01" href="#collapse01-1">About League!</a>
+						</div>
+						<div id="collapse01-1" class="panel--body panel-collapse collapse">
+							With any financial product that you buy, it is important that you know you are getting the best advice from a reputable company.
+						</div>
+					</div>
+
+					<!-- Panel 02 -->
+					<div class="panel">
+						<div class="panel--heading">
+							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion01" href="#collapse01-2">Our Mission!</a>
+						</div>
+						<div id="collapse01-2" class="panel--body panel-collapse collapse in">
+							Trends, vision dominates a lot of our subconscious interpretation of the world around us. On top it, pleasing images.
+						</div>
+					</div>
+
+					<!-- Panel 03 -->
+					<div class="panel">
+						<div class="panel--heading">
+							<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion01" href="#collapse01-3">Our Goals!</a>
+						</div>
+						<div id="collapse01-3" class="panel--body panel-collapse collapse">
+							Trends, vision dominates a lot of our subconscious interpretation of the world around us. On top it, pleasing images.
+						</div>
+					</div>
+				</div>
+				<!-- End .Accordion-->
+			</div><!-- .col-md-4 end -->
+			<div class="col-xs-12 col-sm-12 col-md-4 testimonial testimonial-boxed testimonial-1 bg-gray">
+				<div id="testimonial-carousel" class="carousel" data-slide="1" data-slide-rs="1" data-autoplay="false" data-nav="false" data-dots="false" data-space="0" data-loop="true" data-speed="3000">
+					<!-- Testimonial #1 -->
+					<div class="testimonial-panel">
+						<div class="testimonial--icon"></div><!-- .testimonial-icon end -->
+						<div class="testimonial--body">
+							<p>My project was a simple & small task, but the persistence and determination of the team turned it into an awesome and great project which make me happy!</p>
+						</div><!-- .testimonial-body end -->
+						<div class="testimonial--meta">
+							<div class="testimonial--meta-img">
+								<img src="assets/images/testimonial/1.png" alt="Testimonial Author">
+							</div>
+							<div class="testimonial--meta-content">
+								<h4>Mahmoud Baghagho</h4>
+								<p>7oroof Agency</p>
+							</div>
+						</div><!-- .testimonial-meta end -->
+					</div><!-- .testimonial-panel end -->
+
+					<!-- Testimonial #2 -->
+					<div class="testimonial-panel">
+						<div class="testimonial--icon"></div><!-- .testimonial-icon end -->
+						<div class="testimonial--body">
+							<p>My project was a simple & small task, but the persistence and determination of the team turned it into an awesome and great project which make me happy!</p>
+						</div><!-- .testimonial-body end -->
+						<div class="testimonial--meta">
+							<div class="testimonial--meta-img">
+								<img src="assets/images/testimonial/2.png" alt="Testimonial Author">
+							</div>
+							<div class="testimonial--meta-content">
+								<h4>ayman fikry</h4>
+								<p>zytheme</p>
+							</div>
+						</div><!-- .testimonial-meta end -->
+					</div><!-- .testimonial-panel end -->
+
+					<!-- Testimonial #3 -->
+					<div class="testimonial-panel">
+						<div class="testimonial--icon"></div><!-- .testimonial-icon end -->
+
+						<div class="testimonial--body">
+							<p>My project was a simple & small task, but the persistence and determination of the team turned it into an awesome and great project which make me happy!</p>
+						</div><!-- .testimonial-body end -->
+						<div class="testimonial--meta">
+							<div class="testimonial--meta-img">
+								<img src="assets/images/testimonial/3.png" alt="Testimonial Author">
+							</div>
+							<div class="testimonial--meta-content">
+								<h4>Fouad badawy</h4>
+								<p>Tie Labs Inc</p>
+							</div>
+						</div><!-- .testimonial-meta end -->
+					</div><!-- .testimonial-panel end -->
+
+
+					<!-- Testimonial #4 -->
+					<div class="testimonial-panel">
+						<div class="testimonial--icon"></div><!-- .testimonial-icon end -->
+						<div class="testimonial--body">
+							<p>My project was a simple & small task, but the persistence and determination of the team turned it into an awesome and great project which make me happy!</p>
+						</div><!-- .testimonial-body end -->
+						<div class="testimonial--meta">
+							<div class="testimonial--meta-img">
+								<img src="assets/images/testimonial/4.png" alt="Testimonial Author">
+							</div>
+							<div class="testimonial--meta-content">
+								<h4>Mostafa Mabrook</h4>
+								<p>Code 125</p>
+							</div>
+						</div><!-- .testimonial-meta end -->
+					</div><!-- .testimonial-panel end -->
+				</div>
+			</div><!-- .col-md-4 end -->
+			<div class="col-xs-12 col-sm-12 col-md-4 skills">
+				<!-- progress 1 -->
+				<div class="progressbar">
+					<div class="progress-title">
+						<span class="title">Marketing</span>
+						<span class="value">95%</span>
+					</div>
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
+						</div>
+					</div>
+				</div>
+				<!-- .progressbar end -->
+
+				<!-- progress 2 -->
+				<div class="progressbar">
+					<div class="progress-title">
+						<span class="title">Development</span>
+						<span class="value">87%</span>
+					</div>
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="87" aria-valuemin="0" aria-valuemax="100">
+						</div>
+					</div>
+				</div>
+				<!-- .progressbar end -->
+
+				<!-- progress 3 -->
+				<div class="progressbar">
+					<div class="progress-title">
+						<span class="title">User Experience</span>
+						<span class="value">81%</span>
+					</div>
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="81" aria-valuemin="0" aria-valuemax="100">
+						</div>
+					</div>
+				</div>
+				<!-- .progressbar end -->
+
+				<!-- progress 4 -->
+				<div class="progressbar">
+					<div class="progress-title">
+						<span class="title">Branding</span>
+						<span class="value">70%</span>
+					</div>
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+						</div>
+					</div>
+				</div>
+				<!-- .progressbar end -->
+				<!-- progress 5 -->
+				<div class="progressbar">
+					<div class="progress-title">
+						<span class="title">Seo</span>
+						<span class="value">90%</span>
+					</div>
+					<div class="progress">
+						<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+						</div>
+					</div>
+				</div>
+				<!-- .progressbar end -->
+			</div><!-- .col-md-4 end -->
+		</div><!-- .row end -->
+	</div><!-- .container end -->
+</section><!-- #about end -->
+<div class="clearfix mb-150"></div>
+<!-- About #2
+============================================= -->
+<section id="about2" class="about bg-white">
+	<div class="container">
+		<div class="row clearfix">
+			<div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">
+				<div class="heading heading-2 mb-50 text--center">
+					<p class="heading--subtitle">Who we are?</p>
+					<h2 class="heading--title heding--center">We Are League Agency</h2>
+					<div class="divider--line divider--center"></div>
+				</div>
+			</div><!-- .col-md-8 end -->
+		</div><!-- .row end -->
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-6">
+				<p class="text-right mb-0">League is not just about graphic design; it's more than that. We offer
+					integral communication services, and we're responsible for our process and results. We thank each of our clients and their projects; thanks to them we have grown and built what we are today!</p>
+			</div><!-- .col-md-6 end -->
+			<div class="col-xs-12 col-sm-12 col-md-6">
+				<p class="mb-0">Our agency can only be as strong as our people and because of this, our team have run their own businesses, designed game changing products, consulted for companies as well as competed collegiately and professionally when it comes to sports.</p>
+			</div>
+		</div>
+		<!-- .row end -->
+	</div>
+	<!-- .container end -->
+</section>
+<!-- #about2 end -->
+<div class="clearfix mb-150"></div>
---
+++ resources/views/pages/contactUs.blade.php
@@ -0,0 +1,66 @@
+<!-- Page Title #1
+============================================= -->
+<section id="page-title" class="page-title">
+	<div class="container-fluid bg-overlay bg-overlay-theme">
+		<div class="bg-section">
+			<img src="assets/images/page-title/title-1.jpg" alt="Background"/>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12">
+				<div class="title title-1 text--center">
+					<div class="title--content">
+						<div class="title--heading">
+							<h1>Contact Form</h1>
+						</div>
+					</div>
+				</div><!-- .page-title end -->
+			</div><!-- .col-md-12 end -->
+		</div><!-- .row end -->
+	</div><!-- .container end -->
+</section><!-- #page-title end -->
+
+<!-- Contact #1
+============================================= -->
+<section id="contact1" class="contact contact-1">
+	<div class="container">
+		<div class="row clearfix">
+			<div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">
+				<div class="heading heading-2 mb-60 text--center">
+					<p class="heading--subtitle">Feel Free to</p>
+					<h2 class="heading--title">Write Us Anytime</h2>
+					<div class="divider--line divider--center">
+					</div>
+				</div>
+			</div><!-- .col-md-8 end -->
+		</div><!-- .row end -->
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">
+				<div class="contact-form">
+					<form class="mb-0">
+						<div class="row">
+							<div class="col-md-6">
+								<input type="text" class="form-control" name="contact-name" id="name" placeholder="First Name:" required>
+							</div>
+							<div class="col-md-6">
+								<input type="text" class="form-control" name="contact-namel" id="name-last" placeholder="Last Name" required>
+							</div>
+							<div class="col-md-6">
+								<input type="email" class="form-control" name="contact-email" id="email" placeholder="Email:" required>
+							</div>
+							<div class="col-md-6">
+								<input type="text" class="form-control" name="contact-phone" id="phone" placeholder="Phone:" required>
+							</div>
+							<div class="col-md-12">
+								<textarea class="form-control" name="contact-message" id="message" rows="2" placeholder="Message" required></textarea>
+							</div>
+							<div class="col-md-12">
+								<input type="submit" value="Send Message" name="submit" class="btn btn--secondary btn--block">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div><!-- .col-md-6 end -->
+		</div>
+	</div>
+</section>
+<!-- #contact1 end -->
