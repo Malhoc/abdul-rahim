031cce23445edcbabb4efc95c77e74ac2d78b39f
Maria Anwar <mariaanwar996@gmail.com>
2023-04-12 17:33:46 +0500

    meta tags added

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,7 @@
         ============================================= -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="robots" content="noindex, nofollow"/>
     <!--IE Compatibility Meta-->
     <meta name="author" content="zytheme"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
